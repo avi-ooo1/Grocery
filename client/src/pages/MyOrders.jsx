6405cb0a1aefe9b194dde669f1139a143f8f77c0
Avinash <sainiavinash1111@gmail.com>
2025-10-26 20:29:41 +0530

    Fix date field from createAt to createdAt in MyOrders.jsx

--- client/src/pages/MyOrders.jsx
+++ client/src/pages/MyOrders.jsx
@@ -4,26 +4,26 @@ import { dummyOrders } from '../assets/assets';
 
 
 const MyOrders = () => {
-    const [myOrders,setMyOrders] = useState([]);
-    const {currency,axios,user} = useAppContext();
+    const [myOrders, setMyOrders] = useState([]);
+    const { currency, axios, user } = useAppContext();
 
-    const fetchMyOrders = async () =>{
+    const fetchMyOrders = async () => {
         try {
-            const {data} = await axios.get('/api/order/user')
+            const { data } = await axios.get('/api/order/user')
             if (data.success) {
                 setMyOrders(data.orders)
             }
         } catch (error) {
             console.log(error);
-            
+
         }
     }
 
-    useEffect(()=>{
+    useEffect(() => {
         if (user) {
             fetchMyOrders()
         }
-    },[user])
+    }, [user])
 
     return (
         <div className='mt-16 pb-16'>
@@ -31,14 +31,14 @@ const MyOrders = () => {
                 <p className='text-2xl font-medium uppercase'>My ordres</p>
                 <div className='w-16 h-0.5 bg-primary rounded-b-full'></div>
             </div>
-            {myOrders.map((order,index)=>(
+            {myOrders.map((order, index) => (
                 <div key={index} className='border border-gray-300 rounder-lg mb-10 p-4 py-5 max-w-4xl'>
                     <p className='flex justify-between md:items-center text-gray-400 md:font-medium max-md:flex-col'>
                         <span>OrderId : {order._id}</span>
                         <span>Payment : {order.paymentType}</span>
                         <span>Total Amount : {currency}{order.amount}</span>
                     </p>
-                    {order.items.map((item,index)=>(
+                    {order.items.map((item, index) => (
                         <div key={index} className={`relative bg-white text-gray-500/70 ${order.items.length !== index + 1 && "border-b"} border-gray-300 flex flex-col md:flex-row md:items-center justify-between p-4 py-5 md:gap-16 w-full max-w-4xl`}>
                             <div className='flex items-center mb-4 md:mb-0'>
                                 <div className='bg-primary/10 p-4 rounded-lg'>
@@ -54,7 +54,7 @@ const MyOrders = () => {
                             <div className='flex flex-col justify-center md:ml-8 mb-4 md:mb-0'>
                                 <p>Quantity:{item.quantity || "1"}</p>
                                 <p>Status:{order.status}</p>
-                                <p>Date:{new Date(order.createAt).toLocaleDateString()}</p>
+                                <p>Date:{new Date(order.createdAt).toLocaleDateString()}</p>
                             </div>
 
                             <p className='text-primary text-lg font-medium'>
